02be62379627f2d31a1cb4bd52f8ac4d8d45886d
Sergey Kh <falko.serega2016@yandex.ru>
2021-01-27 22:38:12 +0300

    finish todo

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import './search-panel.css';
+
+export default class SearchPanel extends Component {
+  
+  state = {
+    term: ''
+  }
+
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({term});
+    this.props.onSearchChange(term);
+  }
+
+  render() {
+    
+    return (
+      <input 
+        className="form-control search-input" 
+        type="text"
+        placeholder='Type here to search'
+        value={this.state.term}
+        onChange={this.onSearchChange}/>
+    )
+  }
+}
---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+import './todo-list-item.css';
+
+export default class TodoListItem extends Component {
+
+  render() {
+    const {label, onDeleted, onToggleDone, onToggleImportant, done, important} = this.props;
+
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += ' done'
+    }
+
+    if (important) {
+      classNames += ' important'
+    }
+
+    return (
+      <span className={classNames}>
+        <span 
+          className="todo-list-item-label" 
+          onClick={onToggleDone}>
+            {label}
+        </span>
+    
+        <button 
+          className="btn btn-outline-success btn-sm float-right" 
+          type="button"
+          onClick={onToggleImportant}>
+            <i className="fa fa-exclamation"></i>
+        </button>
+        <button 
+          className="btn btn-outline-danger btn-sm float-right" 
+          type="button"
+          onClick={onDeleted}>
+            <i className="fa fa-trash-o"></i>
+        </button>
+      </span>
+    )
+  }
+}
